1b42b3c022cc773faf79a37528cf2412f8f8e0cc
RaikkoP <raikko.prants@gmail.com>
2023-10-03 10:41:44 +0300

    edited commit

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import {Text, SafeAreaView} from 'react-native';
-import video from './assets/cat-scary.mp4';
-import Video from 'react-native-video';
 
 const App = () => {
   return <Text>HiHiHiHaa</Text>;
